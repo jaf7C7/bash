4d3d2f7b15f7b18cfad7f10a4b8c9346bdb68bde
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-10-06 22:15:11 +0100

    Delete old files

--- bashrc.d/get_pid.bash
+++
@@ -1,4 +0,0 @@
-get_pid() {
-	# Usage: get_pid <progname>
-	ps ux | sed -n "1p;/[${1%${1#?}}]${1#?}/p" | less -XS
-}
--- bashrc.d/traceinput.bash
+++
@@ -1,4 +0,0 @@
-trace_input() {
-	# Usage: trace_input <pid>
-	sudo strace -f -e 'trace=write,read' -e write=all -e read=all -p "$1" 2>&1 | grep -v EAGAIN
-}
