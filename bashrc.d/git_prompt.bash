a6565902de5e42fb3dd57fc07f340fef61823823
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-10-06 22:16:08 +0100

    Show current commit when not on a branch

--- bashrc.d/git_prompt.bash
+++ bashrc.d/git_prompt.bash
@@ -2,9 +2,14 @@ __git_ps1() {
 	# A simplified (and much quicker) version of the prompt which ships
 	# with `git(1)`.
 	# Usage: Add `$(__git_ps1)` to your PS1 or PROMPT_COMMAND string.
-	local branch=$(git branch --show-current 2>/dev/null)
-	if [[ -n $branch ]]
+	if ! git status -s &>/dev/null
 	then
-		printf ' (%s)' "${branch:-'???'}"
+		return
 	fi
+	local current=$(git branch --show-current)
+	if [[ -z $current ]]
+	then
+		current=$(git rev-parse --short HEAD)
+	fi
+	printf ' (%s)' "$current"
 }
