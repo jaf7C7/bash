42439d43abd7e9fb41e2404e44d92fe593e1d329
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-11-01 02:32:51 +0000

    Add check for being ahead of origin

--- functions/gitcheck.bash
+++ functions/gitcheck.bash
@@ -5,30 +5,35 @@ gitcheck() {
 	# not reported.
 	#
 	find ~ -type d -name .git -exec sh -c '
-		is_git_controlled() {
-			git status -s >/dev/null 2>&1
-		}
+		git_controlled() {
+			git status -s
+		} >/dev/null 2>&1
 
 		untracked_files() {
 			git ls-files --other --directory --exclude-standard |
 			grep -q '.'
-		}
+		} >/dev/null 2>&1
 
 		uncommitted_changes() {
 			! git diff --quiet >/dev/null 2>&1
-		}
+		} >/dev/null 2>&1
+
+		ahead_of_master() {
+			git log --oneline origin/master..@ | grep -q '.'
+		} >/dev/null 2>&1
 
 		print_status() {
 			printf "\e[1;34m%s\e[m\n" "$PWD"
-			git status -s
-			echo
+			git status
 		}
 
 		cd $(dirname $0) || exit
 
-		if
-			is_git_controlled &&
-			{ uncommitted_changes || untracked_files ; }
+		if git_controlled && {
+			uncommitted_changes ||
+			untracked_files ||
+			ahead_of_master
+		}
 		then
 			print_status
 		fi
